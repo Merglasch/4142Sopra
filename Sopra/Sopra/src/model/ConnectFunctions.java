1351e8b14232ec4270a8558efd4b69a104af4e6a
Daniel <metzger_daniel@web.de>
2013-05-06 12:38:31 +0200

    derbyjar

---
+++ Sopra/Sopra/src/model/ConnectFunctions.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectFunctions {
+
+	private static String dbURL = "jdbc:derby:MyDB;create=true";
+	public static Connection con = null;
+
+	public static void createConnection() {
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+			con = DriverManager.getConnection(dbURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void shutdown() {
+		try {
+			if (con != null) {
+				DriverManager.getConnection(dbURL + ";shutdown=true");
+				con.close();
+			}
+		} catch (Exception e) {
+			
+		}
+	}
+}
---
+++ Sopra/Sopra/src/model/main.java
@@ -0,0 +1,18 @@
+package model;
+
+import model.account.Login;
+import model.account.Register;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Login.login("hans@dampf.de", "seppal"));
+		//registert einen user
+		//Register.registerthis("mein@mail.de","Herrman","Dieter","mieter",1,"Elektrotechnik");
+	}
+
+}
