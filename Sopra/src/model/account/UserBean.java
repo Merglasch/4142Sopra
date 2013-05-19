737ab6803cb855a3c67e25e43918e47d2f6f527c
David Klein <david.klein@uni-ulm.de>
2013-05-19 20:00:27 +0200

    Umstieg auf Dynamic Web
    Komplette Projektstruktur auf Dynamic Web umgestellt. Enthält login und
    logged In Html Ansichten und weitere noch unangepasste xhtml Seiten

---
+++ Sopra/src/model/account/UserBean.java
@@ -0,0 +1,19 @@
+package model.account;
+
+public class UserBean {
+	String myName="Heinz Dieter";
+
+	/**
+	 * @return the myName
+	 */
+	public String getMyName() {
+		return myName;
+	}
+
+	/**
+	 * @param myName the myName to set
+	 */
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+}
