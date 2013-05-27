a346f9ffc37c2070ad52e95a63d0ea08b511b3f4
Daniel <metzger_daniel@web.de>
2013-05-27 14:02:05 +0200

    Admin DB-Methoden
    Hinzufügen von DB-Methoden:
    createUser()
    updateUser()
    deleteUser()

--- Sopra/src/model/main.java
+++ Sopra/src/model/main.java
@@ -11,7 +11,7 @@ public class main {
 	 * @param args
 	 * @throws NoSuchAlgorithmException 
 	 */
-	public static void main(String[] args) throws NoSuchAlgorithmException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(DBMethoden.login("hans@dampf.de", "seppal"));
 		System.out.println(new Kodierer().code("Hallo"));
