4e0e24fb0cbee2a95414f80499b65d7e12b45cf3
Magnus <magnus.weber@uni-ulm.de>
2013-06-16 18:14:55 +0200

    hinzugefuegt: modulHandbuchBean, geaender, ModelBean und faces-config
    hinzugefuegt: modulHandbuchBean, geaender, ModelBean und faces-config

--- Sopra/src/model/ModelBean.java
+++ Sopra/src/model/ModelBean.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import klassenDB.Modul;
+import klassenDB.Modulhandbuch;
 
 public class ModelBean {
+	//attribute modulhandbuch
 	private String studienabschluss;
-	private String studiengang;
+	private String studiengang; 
 	private String pruefungsordnung;
+	
+	//attribute modul
 	private int nummer;
 	private String modulName;
-	private List<Modul> modulListe;
-
+	
+	String[] handbuchverwalter; //[handbuchID, MODULNAME, abschluss, studiengang, pruefungordnung]
+	private List<Modul> modul;
+	
+//	private List<Modulhandbuch> modulHandbuchListe;
+//	private List<Modul> modul;
+//	private List<Object[][]> handbuchverwalter;
 	
 	public ModelBean(String studienabschluss, String studiengang,
 			String pruefungsordnung, int nummer, String modulName) {
@@ -27,22 +36,66 @@ public class ModelBean {
 		super();
 	}
 
-
+	//liefert liste von modulhandbuechern, modulhandbuecher haben listen von ihren modulen
+	
 	public String sucheModul(){
-		modulListe = new ArrayList<Modul>();
+		//DB Methode aufrufen, die passende modulnamen zur suchanfrage liefert, als String[]
 		
-		///// Abfrage in DB
-		/// modulListe erstellen
+		// antwortseite zeigt nur daten aus handbuchverwalter, 
+		// auf antwortseite neue suche zu dem ausgewaehlten modulnamen
 		
-		// hier: dummy Modul zur
+		//test
+		handbuchverwalter[0] = "NR;Studienabschluss;Studiengang;Pruefungsordnung";
+		handbuchverwalter[1] = "132 ;Studienabschluss;Studiengang;2323";
 		
-		Modul bsp = new Modul();
-		bsp.setModulname("modulname");
-		bsp.setInhalt("inhalt");
-		modulListe.add(bsp);
+		return"Antwortseite1";
+	}
+		
+	
+	
+	
+	
+	
+/*	public String sucheModul(){
+		modulHandbuchListe = new ArrayList<Modulhandbuch>();
+//		modul = new ArrayList<Modul>();
+//		handbuchverwalter = new ArrayList<Object[][]>();
+		handbuchverwalter = new ArrayList<Object[][]>();
+		
+		// DB Methode, die Handbuecher liefert und in modulHandbuchListe speichert
+		//methodenaufruf
+		Modulhandbuch mhb1 = new Modulhandbuch();
+		Modulhandbuch mhb2 = new Modulhandbuch();
+		modulHandbuchListe.add(mhb1);
+		modulHandbuchListe.add(mhb2);
+		
+		
+		Object[][] objects;
+		if(modulHandbuchListe.size() < 0){
+			//nichts
+		}else{
+			
+			for(int i =0; i < modulHandbuchListe.size(); i++){
+				
+				//DB Methode, die module zu uebergebenem handbuch liefert
+				//methodenaufruf liefert liste von modulen
+				// modul = ...Methode...
+				
+				//hier: test
+				modul = new ArrayList<Modul>();
+				Modul bspMod = new Modul();
+				bspMod.setInhalt("Inhalt modul: "+i);
+				modul.add(bspMod);
+				
+				objects = new Object [][];
+				
+				handbuchverwalter;
+			}
+			
+		}
 		
 		return "Antwortseite";
-	}
+	}*/
 	
 	public String sucheModul(String studienabschluss, String studiengang,
 			String pruefungsordnung, int nummer, String modulName){
@@ -102,8 +155,6 @@ public class ModelBean {
 	public void setModulName(String modulName) {
 		this.modulName = modulName;
 	}
-	public List<Modul> getModulListe() {
-		return modulListe;
-	}
+
 	
 }
