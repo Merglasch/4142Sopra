9fc8f4750c2d23974ab79543cf8b6452fe002071
Jonathan <jo.eisenbeiss@gmx.net>
2013-06-28 13:03:35 +0200

    ID Generator

--- Sopra/src/model/IDGenerator.java
+++ Sopra/src/model/IDGenerator.java
@@ -1,26 +1,31 @@
 package model;
 
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 
 @Stateless
 public class IDGenerator {
 	
 	@PersistenceContext
 	private static EntityManager ems;
-	
-	@PersistenceContext
-	private EntityManager em;
-	
-	public int getID() {
-		//bei Erweiterung der DB hier mehr IDs ueberpruefen
-		int uid = em.createQuery("SELECT u.uid FROM User u").getMaxResults();
-		int mid = em.createQuery("SELECT m.modulid FROM Modul m").getMaxResults();
-		int mhid = em.createQuery("SELECT mh.handbuchid FROM Modulhandbuch mh").getMaxResults();
-		int nid = em.createQuery("SELECT n.nachrichtid FROM Benachrichtigung n").getMaxResults();
 		
-		id = Math.max(Math.max(Math.max(uid,mid),mhid),nid);
+	//durchsucht DB nach hoechster id und liefert dann id+1 zurueck
+	public static int getID() {
+		Integer uid=0;
+		Integer mid=0;
+		Integer mhid=0;
+		Integer nid=0;
+		//bei Erweiterung der DB hier mehr IDs ueberpruefen
+		try{
+			uid = ems.createQuery("SELECT MAX(u.uid) FROM User u", Integer.class).getSingleResult();
+			mid = ems.createQuery("SELECT MAX(m.modulid) FROM Modul m", Integer.class).getSingleResult();
+			mhid = ems.createQuery("SELECT MAX(mh.handbuchid) FROM Modulhandbuch mh", Integer.class).getSingleResult();
+			nid = ems.createQuery("SELECT MAX(n.nachrichtid) FROM Benachrichtigung n", Integer.class).getSingleResult();
+		}catch(Exception e){
+			
+		}
+		
+		Integer id = Math.max(Math.max(Math.max(uid,mid),mhid),nid)+1;
 		return id;
 	}
 	
