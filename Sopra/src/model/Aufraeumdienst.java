8783f702411c8ba2c20439a3923ed7ac2e94c892
Daniel <metzger_daniel@web.de>
2013-06-30 13:09:53 +0200

    Aufräumdienst

--- Sopra/src/model/Aufraeumdienst.java
+++ Sopra/src/model/Aufraeumdienst.java
@@ -1,36 +1,21 @@
 package model;
 
-
-import java.sql.Date;
-import java.util.List;
-
 import javax.persistence.EntityManager;
-
-import model.modules.ModuleService;
-
-import klassenDB.Modul;
-
-import model.modules.ModuleService;
 
 public class Aufraeumdienst {
 	private EntityManager em;
 	
-	private List<Modul> moduleList;
-	private Date now = null;
-	private String minTime;
-	
-	public void dateToString() {
-		
-			
-	}
-	
 	public void aufraumer() {
 		String sysDate;
-		long year = 31556952000L;
+		final long year = 31556952000L;
 		long startTime = System.currentTimeMillis() -year;
 		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		sysDate = sdf.format(startTime);
 		
+		em.createQuery("Delete FROM Modul WHERE freigegeben = 0 AND zeitstempel < :sysDate")
+		.setParameter("sysDate",sysDate);
 		
+		em.createQuery("Delete FROM Modulhandbuch WHERE freigegeben = 0 AND zeitstempel < :sysDate")
+		.setParameter("sysDate",sysDate);
 	}
 }
