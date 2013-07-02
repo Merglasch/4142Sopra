ae6fb866ec3c30b9e68a23a1c73747c6501dcd80
inna <inna.duester@uni-ulm.de>
2013-07-02 16:30:58 +0200

    stichtag setzen update

--- Sopra/src/model/stichtag/StichtagBean.java
+++ Sopra/src/model/stichtag/StichtagBean.java
@@ -9,6 +9,7 @@ public class StichtagBean {
 
 	public StichtagBean() {
 		super();
+		stichtagStatus = "Bitte geben Sie ein Datum ein";
 	}
 	
 	@EJB
@@ -19,24 +20,51 @@ public class StichtagBean {
 	private String stichtagStatus = "";
 	private boolean stichtagErfolgreich = false;
 	
+	/**
+	 * @return Stichtag hinzufügen
+	 */
 	public String updateStichtag(){
-		stichtagErfolgreich = stichtagService.updateStichtag(selectStichtag);
-
-		if(stichtag==null && stichtagErfolgreich){
-			stichtagService.updateStichtag(selectStichtag);
-			stichtagStatus="Stichtag wurde erfolgreich hinzugefügt";
-			return "stichtagSetzen";
+		int laengeStichtag = selectStichtag.length();
+		
+		//schaut ob der Stichtag nur Zahlen enthält
+		String[] splitStichtag = selectStichtag.split("\\.");
+		for(int i=0; i<splitStichtag.length; i++){
+			try {
+				int split = Integer.parseInt(splitStichtag[i]);
+			}
+			 catch (NumberFormatException e) {
+				 stichtagStatus="Bitte nur Zahlen nach diesem Format: dd.mm.yyyy eingeben";
+			}
 		}
 		
-		if(stichtagErfolgreich){
-			stichtagStatus="Der Stichtag wurde vom " + stichtag + "auf den " + selectStichtag + "erfolgreich geändert";
-			return "stichtagSetzen";
+		//schaut ob das Format des Datums eingehalten wurde
+		if(laengeStichtag!=10){
+			stichtagStatus="Bitte das Datum nach diesem Format: dd.mm.yyyy eingeben";
 		}
+		
+		//wenn es noch keinen Stichtag gibt
+		else if(stichtag == null){
+			stichtagErfolgreich=stichtagService.updateStichtag(selectStichtag);
+			if(stichtagErfolgreich)
+				stichtagStatus="Stichtag wurde erfolgreich hinzugefügt";
+			else
+				System.out.println("Fehler beim Stichtag setzen (Stichtag==null)");
+		}
+		
+		//wenn ein Stichtag bereits in der Datenbank existiert
+		else if(stichtag != null){
+			stichtagErfolgreich = stichtagService.updateStichtag(selectStichtag);
+			if(stichtagErfolgreich)
+				stichtagStatus="Der Stichtag wurde vom " + stichtag + "auf den " + selectStichtag + "erfolgreich geändert";
+			else
+				System.out.println("Fehler beim Stichtag setzen: Stichtag wurde nicht upgedated");
+		}
+		
 		else{
-			System.out.println("Fehler bei Stichtag setzen");
-			return "stichtagSetzen";
+			System.out.println("Fehler bei Stichtag setzen (Allgemein)");
 		}
 	
+		return "stichtagSetzen";
 	}
 	
 
