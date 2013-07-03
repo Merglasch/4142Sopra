75aba25e04efb311a7ad6d470a67d8f466d78afe
Jonathan <jo.eisenbeiss@gmx.net>
2013-07-03 14:51:20 +0200

    StellvertreterService eingefügt

---
+++ Sopra/src/model/account/StellvertreterService.java
@@ -0,0 +1,57 @@
+package model.account;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import klassenDB.Stellvertreter;
+import klassenDB.StellvertreterPK;
+import klassenDB.User;
+
+@Stateless
+public class StellvertreterService {
+	
+	@PersistenceContext(name="SopraPU")
+	private EntityManager em;
+
+	public List<Integer> getHauptPers(int stvid){
+		return em.createNativeQuery("SELECT hauptpers FROM Stellvertreter WHERE stv=?")
+				.setParameter(1, stvid)
+				.getResultList();
+	}
+	
+	public List<User> getStellvertreter(User u){
+		int hauptPers = u.getUid();
+		List<Integer> stvIDs = em.createQuery("SELECT stv FROM Stellvertreter WHERE hauptPers = ?", Integer.class)
+				.setParameter(1, hauptPers)
+				.getResultList();
+		List<User> resultList = new LinkedList<User>();
+		for(int stvID : stvIDs){
+			resultList.add(em.createQuery("SELECT u FROM User u WHERE u.id = :stvID", User.class)
+					.setParameter("stvID", stvID)
+					.getSingleResult());
+		}
+		return resultList;
+	}
+	
+	public boolean setStellvertreter(User hauptPers, User stv){
+		System.out.println("Methode stellvertreter");
+		boolean success = true;
+		try{
+			StellvertreterPK tmp = new StellvertreterPK();
+			tmp.setHauptpers(hauptPers.getUid());
+			tmp.setStv(stv.getUid());
+			Stellvertreter result = new Stellvertreter();
+			result.setId(tmp);
+			em.persist(result);
+		}catch(Exception e){	
+			success = false;
+			e.printStackTrace();
+		}
+		return success;
+	}
+	
+}
