fd67002e98ec61b17df32bae282b9d2d2515e976
Jonathan <jo.eisenbeiss@gmx.net>
2013-07-04 11:24:50 +0200

    getModulTree(Mh,Fach), getFachTree(Mh)
    Methoden für die neue Baumstruktur mit Fächern

--- Sopra/src/model/TreeService.java
+++ Sopra/src/model/TreeService.java
@@ -1,10 +1,15 @@
 package model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import klassenDB.Fach;
+import klassenDB.Modul;
+import klassenDB.Modulhandbuch;
 
 @Stateless
 public class TreeService {
@@ -37,6 +42,32 @@ public class TreeService {
 		return em.createQuery("SELECT DISTINCT mh.pruefungsordnung FROM Modulhandbuch mh", String.class).getResultList();
 	}
 
-
+	public List<Fach> getFachTree(Modulhandbuch mh){
+		int mhid = mh.getHandbuchid();
+		List<Fach> resultList = new LinkedList<Fach>();
+		List<Integer> fachIDs = em.createNativeQuery("SELECT fID FROM Handbuchverwalter WHERE handbuchID = ?")
+								.setParameter(1, mhid)
+								.getResultList();
+		for(int id : fachIDs){
+			resultList.add(em.find(Fach.class, id));
+		}
+		return resultList;
+	}
+	
+	public List<Modul> getModulTree(Modulhandbuch mh, Fach f){
+		int mhid=mh.getHandbuchid();
+		int fachid=f.getFid();
+		List<Modul> resultList = new LinkedList<Modul>();
+		List<Integer> ModulIDs = em.createNativeQuery("SELECT modulID FROM Handbuchverwalter " +
+				"WHERE handbuchID=? AND fID=?")
+				.setParameter(1, mhid)
+				.setParameter(2, fachid)
+				.getResultList();
+		
+		for(int id : ModulIDs){
+			resultList.add(em.find(Modul.class, id));
+		}
+		return resultList;
+	}
 	
 }
